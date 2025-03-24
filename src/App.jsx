781ca0d1f5180f2ce7c74a69c6df69e77f12cc59
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-03-24 15:19:43 +0000

    create file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route to="/" element={HomePage} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
