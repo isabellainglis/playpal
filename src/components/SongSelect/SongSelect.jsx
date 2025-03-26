14709813ceeb33a9d65b44c525750d5df2fee969
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-03-26 16:40:25 +0000

    add axios call for songs data

--- src/components/SongSelect/SongSelect.jsx
+++ src/components/SongSelect/SongSelect.jsx
@@ -1,15 +1,15 @@
 import "./SongSelect.scss";
 
-export default function SongSelect() {
+export default function SongSelect({ handleSongSelection, song }) {
   return (
-    <div className="song">
+    <div className="song" onClick={() => handleSongSelection(song)}>
       <div className="song__container">
         <div className="song__wrapper song__wrapper--left">
-          <div className="song__info song__info--name">Song Name</div>
-          <div className="song__info song__info--artist">Artist Name</div>
+          <div className="song__info song__info--name">{song.name}</div>
+          <div className="song__info song__info--artist">{song.artist}</div>
         </div>
         <div className="song__wrapper">
-          <div className="song__info">song length</div>
+          <div className="song__info">{song.length}</div>
         </div>
       </div>
     </div>
--- src/pages/SongSelectPage/SongSelectPage.jsx
+++ src/pages/SongSelectPage/SongSelectPage.jsx
@@ -1,7 +1,38 @@
 import "./SongSelectPage.scss";
 import SongSelect from "../../components/SongSelect/SongSelect";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function SongSelectPage() {
+  const [songs, setSongs] = useState(null);
+  const [selectedSong, setSelectedSong] = useState(null);
+
+  const handleSongSelection = async (song) => {
+    setSelectedSong(song);
+    console.log(selectedSong);
+  };
+
+  const fetchSongs = async () => {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/songs`
+      );
+
+      setSongs(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  if (!songs) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <main className="song-select">
       <section className="song-select__select-container">
@@ -9,46 +40,83 @@ export default function SongSelectPage() {
           <h1 className="song-select__header">SONGS</h1>
         </div>
         <div className="song-select__songs-wrapper">
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
-          <SongSelect />
+          {songs.map((song) => {
+            return (
+              <SongSelect
+                handleSongSelection={handleSongSelection}
+                key={song.id}
+                song={song}
+              />
+            );
+          })}
         </div>
       </section>
       <section className="song-select__details-container">
         <div className="song-select__title"></div>
-        <div className="song-select__song-details">
-          <div className="song-select__details-outer-wrapper">
-            <div className="song-select__detail-wrapper--one">
-              <div className="song-select__song-info song-select__song-info--med">
-                ★★★★★
+        {selectedSong && (
+          <div className="song-select__song-details">
+            <div className="song-select__details-outer-wrapper">
+              <div className="song-select__detail-wrapper--one">
+                {selectedSong.difficulty === 1 && (
+                  <div className="song-select__song-info song-select__song-info--med">
+                    ★☆☆☆☆
+                  </div>
+                )}
+                {selectedSong.difficulty === 2 && (
+                  <div className="song-select__song-info song-select__song-info--med">
+                    ★★☆☆☆
+                  </div>
+                )}
+                {selectedSong.difficulty === 3 && (
+                  <div className="song-select__song-info song-select__song-info--med">
+                    ★★★☆☆
+                  </div>
+                )}
+                {selectedSong.difficulty === 4 && (
+                  <div className="song-select__song-info song-select__song-info--med">
+                    ★★★★☆
+                  </div>
+                )}
+                {selectedSong.difficulty === 5 && (
+                  <div className="song-select__song-info song-select__song-info--med">
+                    ★★★★★
+                  </div>
+                )}
+
+                <div className="song-select__song-img">
+                  <img
+                    className="song-select__img"
+                    src={`${import.meta.env.VITE_BACKEND_URL}/images/${
+                      selectedSong.img
+                    }.png`}
+                    alt={selectedSong.img_alt}
+                  />
+                </div>
               </div>
-              <div className="song-select__song-img">
-                <img className="song-select__img" src="" alt="" />
+            </div>
+            <div className="song-select__details-outer-wrapper">
+              <div className="song-select__detail-wrapper--two">
+                <div className="song-select__song-info--small">
+                  {selectedSong.length}
+                </div>
+                <div className="song-select__song-info--small">
+                  {selectedSong.tempo}bpm
+                </div>
+              </div>
+              <div className="song-select__detail-wrapper--three">
+                <div className="song-select__song-info song-select__song-info">
+                  {selectedSong.name}
+                </div>
+                <div className="song-select__song-info song-select__song-info">
+                  {selectedSong.artist}
+                </div>
+                <Link to="/play">
+                  <button className="song-select__btn">PLAY</button>
+                </Link>
               </div>
             </div>
           </div>
-          <div className="song-select__details-outer-wrapper">
-            <div className="song-select__detail-wrapper--two">
-              <div className="song-select__song-info--small">Length: </div>
-              <div className="song-select__song-info--small">Tempo: </div>
-            </div>
-            <div className="song-select__detail-wrapper--three">
-              <div className="song-select__song-info song-select__song-info">
-                Song name
-              </div>
-              <div className="song-select__song-info song-select__song-info">
-                Artist name
-              </div>
-            </div>
-          </div>
-        </div>
+        )}
       </section>
     </main>
   );
