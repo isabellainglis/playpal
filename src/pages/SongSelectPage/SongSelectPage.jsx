6fdd81c4eb7f800fe20540988c4450accee9a7f0
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-03-26 11:31:24 +0000

    add basic styling for song details

--- src/pages/SongSelectPage/SongSelectPage.jsx
+++ src/pages/SongSelectPage/SongSelectPage.jsx
@@ -3,34 +3,40 @@ import "./SongSelectPage.scss";
 export default function SongSelectPage() {
   return (
     <main className="song-select">
-      <section className="song-select__details-container">
-        <div className="song-select__title"></div>
-        <div className="song-select__song-details">
-          <div className="song-select__detail-wrapper">
-            <div className="song-select__song-info">Length: </div>
-            <div className="song-select__song-info">Tempo: </div>
-          </div>
-          <div className="song-select__detail-wrapper">
-            <div className="song-select__song-info song-select__song-info--small">
-              Song name
-            </div>
-            <div className="song-select__song-info song-select__song-info--small">
-              Artist name
-            </div>
-          </div>
-          <div className="song-select__detail-wrapper">
-            <div className="song-select__song-info">★★★★★</div>
-            <div className="song-select__song-img">
-              <img className="song-select__img" src="" alt="" />
-            </div>
-          </div>
-        </div>
-      </section>
       <section className="song-select__select-container">
         <div className="song-select__header">
           <h1>SONGS</h1>
         </div>
         <div className="song-select__songs-wrapper"></div>
+      </section>
+      <section className="song-select__details-container">
+        <div className="song-select__title"></div>
+        <div className="song-select__song-details">
+          <div className="song-select__details-outer-wrapper">
+            <div className="song-select__detail-wrapper--one">
+              <div className="song-select__song-info song-select__song-info--med">
+                ★★★★★
+              </div>
+              <div className="song-select__song-img">
+                <img className="song-select__img" src="" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="song-select__details-outer-wrapper">
+            <div className="song-select__detail-wrapper--two">
+              <div className="song-select__song-info--small">Length: </div>
+              <div className="song-select__song-info--small">Tempo: </div>
+            </div>
+            <div className="song-select__detail-wrapper--three">
+              <div className="song-select__song-info song-select__song-info">
+                Song name
+              </div>
+              <div className="song-select__song-info song-select__song-info">
+                Artist name
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
