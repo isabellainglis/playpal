74852836dcdd6db6fd8fa51d94b59b8c3c05e886
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-03-31 19:25:18 +0100

    render lyrics with basic styling

--- src/components/LyricsDisplay/LyricsDisplay.jsx
+++ src/components/LyricsDisplay/LyricsDisplay.jsx
@@ -1,13 +1,14 @@
 import "./LyricsDisplay.scss";
+import { v4 as uuidv4 } from "uuid";
 
 export default function LyricsDisplay({ chords, section }) {
   return (
-    <div>
+    <div className="lyrics">
       <div>{section}</div>
       {chords.map((singleLyric) => {
         return (
-          <span>
-            <span className="lyrics__lyric">{singleLyric.lyrics}</span>
+          <span key={uuidv4()} className="lyrics__lyric-container">
+            {singleLyric.lyrics}
             <span className="lyrics__chord">{singleLyric.name}</span>
           </span>
         );
