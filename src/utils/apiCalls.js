3d3cbd87b08cf71678c98b8a9b41c46ebaa94a3a
issy-inglis@hotmail.co.uk <issy-inglis@hotmail.co.uk>
2025-04-02 15:04:00 +0100

    separate api calls into utils

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+
+const fetchAllSongs = async () => {
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/songs`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const fetchAllChords = async () => {
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/chords`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const fetchAllSongChords = async () => {
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/chords/all`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const fetchSingleSongDetails = async (songId) => {
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/songs/${songId}/details`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const fetchSingleSongChords = async (songId) => {
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/songs/${songId}/chords`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const fetchSingleSongLyrics = async (songId) => {
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/songs/${songId}/sections`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export {
+  fetchAllSongs,
+  fetchAllChords,
+  fetchAllSongChords,
+  fetchSingleSongDetails,
+  fetchSingleSongChords,
+  fetchSingleSongLyrics,
+};
